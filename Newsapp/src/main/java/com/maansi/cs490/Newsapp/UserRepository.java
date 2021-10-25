9e68f204e1264322bd54b813fb6d85484492f0a5
maansisolanki <90276728+maansisolanki@users.noreply.github.com>
2021-10-24 21:47:08 -0400

    Delete Newsapp directory

--- Newsapp/src/main/java/com/maansi/cs490/Newsapp/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.maansi.cs490.Newsapp;
-import org.springframework.data.repository.CrudRepository;
-import com.maansi.cs490.Newsapp.Tbl_signup;
-
-
-public interface UserRepository extends CrudRepository<Tbl_signup, Integer>{
-
-}
